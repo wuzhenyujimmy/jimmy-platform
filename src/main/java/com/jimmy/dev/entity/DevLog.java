b70271f02a39991b50613d75fcb7872c7d12c417
jimmy <jaychou19910110@126.com>
2014-04-18 19:51:09 +0800

    recreate project at home

--- src/main/java/com/jimmy/dev/entity/DevLog.java
+++ src/main/java/com/jimmy/dev/entity/DevLog.java
@@ -1,6 +1,9 @@
 package com.jimmy.dev.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 import com.jimmy.base.common.Article;
@@ -18,7 +21,8 @@ public class DevLog extends BaseEntity {
 
     /* Setter and getter start */
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn
     public Article getArticle() {
         return article;
     }
