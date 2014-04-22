e9325b77f9b516081f30cd01f7db95ca082cd3ba
jimmy <jaychou19910110@126.com>
2014-04-22 17:14:32 +0800

    Signed-off-by: jimmy <jaychou19910110@126.com>

--- src/main/java/com/jimmy/module/dev/DevLogService.java
+++
@@ -1,7 +0,0 @@
-package com.jimmy.module.dev;
-
-import com.jimmy.base.db.BaseDao;
-
-public interface DevLogService extends BaseDao<DevLog> {
-
-}
--- src/main/java/com/jimmy/module/dev/DevLogServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.jimmy.module.dev;
-
-import com.jimmy.base.db.DaoSupport;
-
-public class DevLogServiceImpl extends DaoSupport<DevLog> implements
-        DevLogService {
-
-}
